8e3ea07cf6255a91b9441ed6fc88532fe5d238e7
ruchita-patil <ruchita.patil@catseyesystems.com>
2020-09-28 23:10:18 +0530

    computing employee wage for months

--- src/main/java/employeeWageComputation.java
+++ src/main/java/employeeWageComputation.java
@@ -7,7 +7,8 @@ import java.util.*;
 public class employeeWageComputation {
 
 	// Constants
-	static final int PRESENT=1,WAGE_PER_HOUR=20,FULL_TIME=1,PART_TIME=0;
+	static final int PRESENT = 1, WAGE_PER_HOUR = 20, FULL_TIME = 1, PART_TIME = 0, WORKING_DAYS_IN_MONTH = 20,
+			MAX_WORKING_HRS = 100;
 
 	public static int generateRandomNumber() {
 		double x = Math.random();
@@ -17,33 +18,53 @@ public class employeeWageComputation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int Worked_Hrs = 0, Day_Salary_Of_Employee = 0;
-		System.out.println("Wecome to employee wage program");
 		
-		int Random_No = (int) (Math.floor(Math.random() * 10) % 2);
+		int Worked_Hrs = 0, Day_Salary_Of_Employee = 0, Month_Salary_Of_Employee = 0;
+		int Emp_Worked_hrs = 0, i, Salary, Total_Salary_Of_Employee = 0;
+		System.out.println("Wecome to employee wage program");
+
+		// int Random_No = (int) (Math.floor(Math.random() * 10) % 2);
 		// random function for check employee is absent or present
 
-		if (PRESENT == Random_No) {
+		for (i = 1; i <= WORKING_DAYS_IN_MONTH && Emp_Worked_hrs <= MAX_WORKING_HRS; i++) {
+			int Job_Type = (int) (Math.floor(Math.random() * 10) % 3);
 			
-			int Job_Type=(int) (Math.floor(Math.random()*10)%2);
-  	  		if(FULL_TIME==Job_Type)
-  	  		{
-  	  			System.out.println("Employee is present full time.");
-  	  			Worked_Hrs=8;
-  	  		}
-  	  		else
-  	  		{
-  	  			System.out.println("Employee is present part time.");
-	  			Worked_Hrs=4;
-  	  		}	
-  		}
-  	  	else
-  	  		
-  			System.out.println("Employee is absent.");
-  		//computing employee wage for one day
-  		Day_Salary_Of_Employee=WAGE_PER_HOUR*Worked_Hrs;
-  		System.out.println("Your one day salary creadited to your account, rupees " +Day_Salary_Of_Employee);
-  		
+			switch (Job_Type) {
+			case FULL_TIME:
+				System.out.println("Employee is present full time.");
+				Worked_Hrs = 8;
+				break;
+				
+			case PART_TIME:
+				System.out.println("Employee is present part time.");
+				Worked_Hrs = 4;
+				break;
+			default:
+				System.out.println("Employee is absent.");
+			}
+
+			
+			if (Emp_Worked_hrs == 96 || Emp_Worked_hrs == 94) {
+				if (Worked_Hrs == 8)
+					break;
+			} else if (Emp_Worked_hrs == 98) {
+				break;
+			}
+			
+			Emp_Worked_hrs += Worked_Hrs;
+			Salary = WAGE_PER_HOUR * Worked_Hrs;
+			Total_Salary_Of_Employee += Salary;
+		}
+
+		
+		System.out.println("Total salary of employee for 100hrs and 20days, rupees " + Total_Salary_Of_Employee);
+		// computing employee wage for one day
+		Day_Salary_Of_Employee = WAGE_PER_HOUR * Worked_Hrs;
+		System.out.println("Your one day salary creadited to your account, rupees " + Day_Salary_Of_Employee);
+		// computing employee wage for month
+		Month_Salary_Of_Employee = WORKING_DAYS_IN_MONTH * Day_Salary_Of_Employee;
+		System.out.println("Your one day salary creadited to your account, rupees " + Month_Salary_Of_Employee);
+
 	}
 
 }
